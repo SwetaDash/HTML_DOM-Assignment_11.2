e7e22d8430839d974a7e87291758b9188431e610
SwetaDash <43117650+SwetaDash@users.noreply.github.com>
2019-04-14 18:45:02 +0530

    Add files via upload

---
+++ Assignment_11.2/script.js
@@ -0,0 +1,6 @@
+function myCreateFunction() {
+    var table = document.getElementById("dynamic-table");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0).style.height = "15px";
+    var cell2 = row.insertCell(1).style.height = "15px";
+}
